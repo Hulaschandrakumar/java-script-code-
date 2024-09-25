56cb72dbe5651a3dd020c9d6b0d17b7a6cf926e5
Hulas Chandra kumar <95746638+Hulaschandrakumar@users.noreply.github.com>
2024-09-25 08:20:13 +0000

    codespace container commit code

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 12114496
+let accountEmail = "hulas.12114496@lpu.in"
+var accountPassword = "12107053"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId = 2 // not allowed
+accountEmail = "pradumankumarbroduly33@gmail.com"
+accountPassword = 7666753
+accountCity = "benguluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+/*
+Prefer not to  use var
+because of issue in block scope and functional scope
+*/
