b1d082056307cbbcf2718776772ee3981c5eaef8
Hulas Chandra kumar <95746638+Hulaschandrakumar@users.noreply.github.com>
2024-09-25 08:42:40 +0000

    datatypes

--- .devcontainer/01_basics/dataTypes.js
+++ .devcontainer/01_basics/dataTypes.js
@@ -3,3 +3,24 @@
 // alert("Hello") // We are using node js not browser;
 
 console.log(3+3)
+let name = "Pradyumn"
+let age = 22
+let isLoggedIn = false
+let state = null
+
+// number => 2 tp power 23
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof "Pradyumn");
+console.log(typeof null);
+console.log(typeof age);
+console.log(typeof undefined);
+
+
